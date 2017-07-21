4f59cfb9a0616c02b775be721541e7af99a75e80
Max Shkurygin <max.atreides@gmail.com>
2017-07-21 12:45:12 -0400

    simplified database model

--- eisenhower_dashboard/matrix/models.py
+++ eisenhower_dashboard/matrix/models.py
@@ -1,27 +1,26 @@
 # -*- coding: utf-8 -*-
 
+from django.conf import settings
 from django.db import models
-
-
-class Quadrant(models.Model):
-    title = models.CharField(max_length=200)
+from django.contrib.auth.models import User
 
 
 class TimeSession(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+
+    QUADRANT_CHOICES = (
+        ('q1', 'q1'),
+        ('q2', 'q2'),
+        ('q3', 'q3'),
+        ('q4', 'q4'),
+    )
+    quadrant = models.CharField(choices=QUADRANT_CHOICES, max_length=2)
+
     start = models.DateTimeField();
     end = models.DateTimeField();
 
-    quadrant = models.ForeignKey(Quadrant)
-
     def __str__(self):
-        return 
+        return f'<TimeSession of {self.user!s} ({self.start!s}, {self.end!s})>'
 
     def __unicode__(self):
-        return 
-
-class Matrix(models.Model):
-    title = models.CharField(max_length=30, null=False)
-    q1 = models.OneToOneField(Quadrant, related_name='q1');
-    q2 = models.OneToOneField(Quadrant, related_name='q2')
-    q3 = models.OneToOneField(Quadrant, related_name='q3')
-    q4 = models.OneToOneField(Quadrant, related_name='q4')
+        return self.__str__()
